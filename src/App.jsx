9044e355a6860a4002a9bd5142dddd962dce0414
adyvka31 <rafifdyvka07@gmail.com>
2026-05-16 20:17:58 +0700

    refactor(App): restructure routing with AnimatedRoutes and improve code organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar/Navbar";
 import { useCursorSpotlight } from "./hooks/useCursorSpotlight";
+import useScrollToTop from "./hooks/useScrollToTop";
 
-function App() {
-  useCursorSpotlight();
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ProjectDetailPage from "./pages/ProjectDetailPage";
+import ExperiencePage from "./pages/ExperiencePage";
+import ExperienceDetailPage from "./pages/ExperienceDetailPage";
+import AchievementsPage from "./pages/AchievementsPage";
+import AchievementDetailPage from "./pages/AchievementDetailPage";
+import CertificatesPage from "./pages/CertificatesPage";
+import CertificateDetailPage from "./pages/CertificateDetailPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+function AnimatedRoutes() {
+  const location = useLocation();
+  useScrollToTop();
 
   return (
-    <Router>
-      <div className="spotlight"></div>
-      <Navbar />
-      <Routes>
+    <AnimatePresence mode="wait" initial={false}>
+      <Routes location={location} key={location.pathname}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
+        <Route path="/projects/:slug" element={<ProjectDetailPage />} />
+        <Route path="/experience" element={<ExperiencePage />} />
+        <Route path="/experience/:slug" element={<ExperienceDetailPage />} />
+        <Route path="/achievements" element={<AchievementsPage />} />
+        <Route path="/achievements/:slug" element={<AchievementDetailPage />} />
+        <Route path="/certificates" element={<CertificatesPage />} />
+        <Route path="/certificates/:slug" element={<CertificateDetailPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
-    </Router>
+    </AnimatePresence>
   );
 }
 
-export default App;
+export default function App() {
+  useCursorSpotlight();
+  return (
+    <BrowserRouter>
+      <div className="spotlight" />
+      <Navbar />
+      <AnimatedRoutes />
+    </BrowserRouter>
+  );
+}
