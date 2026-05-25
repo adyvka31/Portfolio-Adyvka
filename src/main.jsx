bed08fbceb9687b2ea61d364e3172fa5470c1091
adyvka31 <rafifdyvka07@gmail.com>
2026-05-25 14:14:27 +0700

    refactor(main.jsx): remove unused font imports to optimize performance

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App.jsx';
 import "@fontsource/geist/400.css";
 import "@fontsource/geist/500.css";
-import "@fontsource/geist/600.css";
-import "@fontsource-variable/geist-mono";
 import "@fontsource/instrument-serif/400-italic.css";
 
 createRoot(document.getElementById('root')).render(
