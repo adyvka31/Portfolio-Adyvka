5621abb7eed3b87bd4f5ccc2ec0d1a4bb4f0b479
adyvka31 <rafifdyvka07@gmail.com>
2026-05-30 07:13:30 +0700

    build(vite): adjust configuration for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
+          if (!id.includes("node_modules")) return;
+          if (id.includes("react-router")) return "router";
           if (
-            id.includes("node_modules/react") ||
-            id.includes("node_modules/react-dom") ||
-            id.includes("node_modules/react-router-dom")
+            id.includes("react-dom") ||
+            id.includes("/react/") ||
+            id.includes("scheduler")
           ) {
-            return "react-vendor";
+            return "react";
           }
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (id.match(/framer-motion|motion-dom|motion-utils/)) {
+            return "motion";
           }
+          return "vendor";
         },
       },
     },
